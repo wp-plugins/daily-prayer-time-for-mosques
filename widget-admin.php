91330d21f4a62d71001f52fc5dc8a167ab8eca19
mmrs151 <mmrs151@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-13 16:53:52 +0000

    Uploading timetable csv and reading data
    git-svn-id: https://plugins.svn.wordpress.org/daily-prayer-time-for-mosques/trunk@1220107 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- widget-admin.php
+++ widget-admin.php
@@ -1,14 +1,66 @@
-<div class="wrap">
-    <h2>Set prayer time for your mosque</h2></br></br>
-	<form name="form1" method="post" action="">
-        <input type="checkbox" name="jamahOnly" value="jamahOnly"/>Display Jamah time only</br>
-        <input type="checkbox" name="hanafiAsr" value="hanafiAsr"/>Display Asr start time according to Hanafi school</br></br>
-        <input type="radio" name="choice" value="vertical"/>Display prayer time vertically</br>
-        <input type="radio" name="choice" value="vertical"/>Display prayer time horizontally</br></br>
+<?php
 
-        Select prayer time csv to upload:
-	    <input type="file" name="fileToUpload" id="fileToUpload">
-	    <input type="submit" value="Upload Prayer Time" name="submit">
+ini_set('auto_detect_line_endings', true);
+
+if (isset($_POST['submit'])) {
+    if ( $_FILES["timetable"]["type"] === "text/csv") {
+
+        $temp = $_FILES["timetable"]["tmp_name"];
+
+        $row = 0;
+
+        if (($handle = fopen($temp, "r")) !== FALSE) {
+            /** skip column headings */
+            fgetcsv($handle);
+
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                $num = count($data);
+                $row++;
+                for ($c=0; $c < $num; $c++) {
+                    if ($c == 0) {
+                        validateDateFormat($data[$c]);
+                    } else {
+                        validateTimeFormat($data[$c]);
+                    }
+                }
+            }
+        }
+        echo $row;
+        fclose($handle);
+    }
+}
+
+/**
+ * @param string $date
+ */
+function validateDateFormat($date)
+{
+//    if (not valid date format) {
+        echo "<h3 class='important'>Invalid Date format</h3>";
+        var_dump($date);
+//    }
+}
+
+/**
+ * @param string $time
+ */
+function validateTimeFormat($time)
+{
+//    if (not valid date format) {
+    echo "<h3 class='important'>Invalid Time format</h3>";
+    var_dump($time);
+//    }
+}
+?>
+
+<div xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html">
+    <h1>Set prayer time for your mosque</h1></br></br>
+    <h2><a href="http://plugins.svn.wordpress.org/masjidnow/trunk/sample.csv"> Download sample csv.</a></h2></br>
+    <h3 class="important">Please update the csv with your mosque's timetable before upload.</h3> </br>
+    <form enctype="multipart/form-data" name="form1" method="post" action="">
+        <h2>Select prayer time csv to upload:</h2>
+        <input type="file" name="timetable" id="timetable">
+        <input type="submit" value="Upload Prayer Time" name="submit">
     </form>
 
 </div>
